944049978d8e37b99db7427d040e24f42733c84a
Stuart Pringle <kaboodleschmitt@hotmail.com>
2026-02-08 18:00:24 -0500

    feat(sdk): add character api base

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const RULES_API_BASE = "https://whisperspace.com/rules-api/latest";
 export const CALC_API_BASE = "https://whisperspace.com/rules-api/calc";
+export const CHARACTER_API_BASE = "https://whisperspace.com/character-api";
